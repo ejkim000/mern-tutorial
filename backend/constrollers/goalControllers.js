14fcd312e7df8ae0348c361a75b827d967651b7b
ejkim000 <developer.ejk@gmail.com>
2023-08-30 13:56:54 -0400

    add errorHandler middleware

--- backend/constrollers/goalControllers.js
+++ backend/constrollers/goalControllers.js
@@ -1,5 +1,8 @@
 // @desc Get goals
 // @route GET /api/goals
+
+const { error } = require("console");
+
 // @access Private
 const getGoals = (req, res) => {
   res.status(200).json({ message: "Get goals" });
@@ -9,8 +12,14 @@ const getGoals = (req, res) => {
 // @route POST /api/goals
 // @access Private
 const setGoal = (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ message: "Set goal" });
+
+  if (!req.body.text) {
+    // res.status(400).json({message: 'Please add text.'})
+    res.status(400);
+    throw new Error('Please add a text field');
+  } else {
+    res.status(200).json({ message: "Set goal" });
+  }
 };
 
 // @desc Update goals
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 3020;
+const { errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 const goalRoutes = require('./routes/goalRoutes');
 
@@ -10,6 +11,8 @@ app.use(express.urlencoded({extended: false})); // For req.params
 
 
 app.use('/api/goals', goalRoutes);
+
+app.use(errorHandler); //to make override default errorhandler
 
 app.listen(PORT, ()=> {
     console.log(`Server startd on port ${PORT}`);
