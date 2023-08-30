214b39b7ee84c33f8e0845ecdd410b413b1e9287
ejkim000 <developer.ejk@gmail.com>
2023-08-30 16:05:51 -0400

    update delete part

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -51,16 +51,13 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
 
-  const goal = await Goal.findById(req.params.id);
+  const deletedGoal = await Goal.findByIdAndRemove(req.params.id);
 
-  console.log('goal:',goal);
-
-  if (!goal) {
+  if (!deletedGoal) {
     res.status(400);
     throw new Error('Goal not found');
   }
 
-  await goal.deleteOne();
   res.status(200).json({id : req.params.id});
 });
 
