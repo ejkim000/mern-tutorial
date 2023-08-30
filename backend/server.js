db6cff7604e663bf700eab45c7a90fd96cad8ab5
ejkim000 <developer.ejk@gmail.com>
2023-08-30 11:42:41 -0400

    setup goal routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hi");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 3020;
+const app = express();
+const goalRoutes = require('./routes/goalRoutes');
+
+
+app.use('/api/goals', goalRoutes);
+
+app.listen(PORT, ()=> {
+    console.log(`Server startd on port ${PORT}`);
+})
