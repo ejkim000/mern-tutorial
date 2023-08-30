612c2837b70d86c5ba5f203513481e0b7579211c
ejkim000 <developer.ejk@gmail.com>
2023-08-30 12:25:27 -0400

    make more clean

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const { getGoals, setGoals, updateGoals, deleteGoals } = require("../constrollers/goalControllers");
+const {
+  getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
+} = require("../constrollers/goalControllers");
 
-router
-.get("/", getGoals)
-.post("/", setGoals)
-.put("/:id", updateGoals)
-.delete("/:id", deleteGoals)
+router.route('/')
+.get(getGoals)
+.post(setGoals);
+
+router.route('/:id')
+.put(updateGoals)
+.delete(deleteGoals);
+
+// router.route('/')
+//   .get("/", getGoals)
+//   .post("/", setGoals)
+//   .put("/:id", updateGoals)
+//   .delete("/:id", deleteGoals);
 
 module.exports = router;
