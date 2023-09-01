b4ff86dfb1b2a23290babfaf8fb3fc0c9ceceeba
ejkim000 <developer.ejk@gmail.com>
2023-09-01 19:20:17 -0400

    add getGoals

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -29,6 +29,28 @@ export const createGoal = createAsyncThunk(
   }
 );
 
+// Get user goals
+export const getGoals = createAsyncThunk(
+    "goals/getAll",
+    async (_, thunkAPI) => {
+      try {
+        // By using thunkAPI, can access to all state
+        const token = thunkAPI.getState().auth.user.token;
+        return await goalService.getGoals(token);
+        
+      } catch (err) {
+        const message =
+          (err.response && err.response.data && err.response.data.message) ||
+          err.message ||
+          err.toString();
+  
+        return thunkAPI.rejectWithValue(message);
+      }
+    }
+  );
+
+  
+
 export const goalSlice = createSlice({
   name: "goal",
   initialState,
@@ -46,6 +68,19 @@ export const goalSlice = createSlice({
         state.goals.push(action.payload);
     })
     .addCase(createGoal.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+    })
+    .addCase(getGoals.pending, (state) => {
+        state.isLoading = true;
+    })
+    .addCase(getGoals.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.goals = action.payload;
+    })
+    .addCase(getGoals.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
